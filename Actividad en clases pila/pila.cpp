f8005e57cb11cc16d02e8fc016ff7cdb01c9b621
KevinQuimuna <121051282+KevinQuimuna@users.noreply.github.com>
2023-05-19 10:17:15 -0500

    .

---
+++ Actividad en clases pila/pila.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void decimalToBinary(int decimal) {
+    int binary[32];
+    int index = 0;
+
+    while (decimal > 0) {
+        binary[index] = decimal % 2;
+        decimal /= 2;
+        index++;
+    }
+
+    cout << "El número binario es: ";
+    for (int i = index - 1; i >= 0; i--) {
+        cout << binary[i];
+    }
+}
+
+int main() {
+    int decimal;
+
+    cout << "Ingrese un número decimal: ";
+    cin >> decimal;
+
+    decimalToBinary(decimal);
+
+    return 0;
+}
+
