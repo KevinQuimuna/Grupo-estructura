f2c738ba94c2380e9b52afbd03bab029f0ec1802
MishellCuasquer <mecuasquer@espe.edu.ec>
2023-05-19 10:31:14 -0500

    Update pila.cpp

--- Actividad en clases pila/pila.cpp
+++ Actividad en clases pila/pila.cpp
@@ -1,30 +1,31 @@
+
 #include <iostream>
 using namespace std;
 
-void decimalToBinary(int decimal) {
-    int binary[32];
+void decimalbinario(int decimal) 
+{
+    int binario[20];
     int index = 0;
 
     while (decimal > 0) {
-        binary[index] = decimal % 2;
+        binario[index] = decimal % 2;
         decimal /= 2;
         index++;
     }
 
-    cout << "El número binario es: ";
+    cout << "El numero binario es: ";
     for (int i = index - 1; i >= 0; i--) {
-        cout << binary[i];
+        cout << binario[i];
     }
 }
 
 int main() {
     int decimal;
 
-    cout << "Ingrese un número decimal: ";
+    cout << "Ingrese un numero decimal: ";
     cin >> decimal;
 
-    decimalToBinary(decimal);
+    decimalbinario (decimal);
 
     return 0;
 }
-
